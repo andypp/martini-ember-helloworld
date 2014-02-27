9b3679538d06b42e8528b59eb05c911f60932e6a
andy@mba <andy.prapanca@gmail.com>
2014-02-27 23:40:20 +0800

    Change theme to blue-white Create skeleton Put in some contents

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,10 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue', 'something'];
+    return [
+      {title: "Flight Price Checker", description: "Something trivial, everyone does this...", url: ""},
+      {title: "Room Finder", description: "Same thing, sigh... no good idea.", url: ""},
+      {title: "Coming soon 1", description: "Still thinking, what might be not too bad.", url: ""},
+      {title: "Coming soon 2", description: "Just to fill up space", url: ""}];
   }
 });
--- server.go
+++ server.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "github.com/codegangsta/martini"
-    "github.com/codegangsta/martini-contrib/render"
-    "html/template"
-    "strings"
+  "github.com/codegangsta/martini"
+  "github.com/codegangsta/martini-contrib/render"
+  "html/template"
+  "time"
 )
 
 type Person struct {
-    Name string
+  Name string
 }
 
-func ToUpper(args ...interface{}) string {
-    return strings.ToUpper(args[0].(string))
+func CurrentYear() int {
+  return time.Now().Year()
 }
 
 func main() {
-    m := martini.Classic()
-    m.Use(render.Renderer(render.Options{
-        Layout: "layout",
-        Funcs: []template.FuncMap{
-            template.FuncMap{"toUpper": ToUpper},
-        },
-        Delims: render.Delims{Left: "{{%", Right: "%}}"},
-    }))
-    m.Get("/", func(r render.Render) {
-        p := Person{Name: "lalalla"}
-        r.HTML(200, "hello", p)
-    })
-    m.Run()
+  m := martini.Classic()
+  m.Use(render.Renderer(render.Options{
+    Layout: "layout",
+    Funcs: []template.FuncMap{
+      template.FuncMap{"currentYear": CurrentYear},
+    },
+    Delims: render.Delims{Left: "{{%", Right: "%}}"},
+  }))
+  m.Get("/", func(r render.Render) {
+    p := Person{Name: "lalalla"}
+    r.HTML(200, "hello", p)
+  })
+  m.Run()
 }
